6b249619340459fb282c886d333eb38794bf28f1
TOKONIH <tokonihstandwell@gmail.com>
2023-05-14 21:16:16 +0100

    i worked on the local storage

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function userData(arr) {
+    let userOutputs = JSON.parse(localStorage.getItem('useOutputs') || '[]')
     let output = ""
     arr.forEach((data) => {
-         output += `
+        output += `
         <div class="details-">
         <p>First Name: ${data.Firstname} </p>
         <p>Last Name:${data.Lastname} </p>
@@ -9,26 +10,43 @@ function userData(arr) {
         <p>Gender: ${data.Gender}</p>
     </div> 
         `;
+        userOutputs.push(output)
+        document.getElementById('details').innerHTML += output
+
+    });
+    localStorage.setItem('userOutputs',JSON.stringify(userOutputs))
+    // localStorage.setItem('userOutput', JSON.stringify(output))
+    // localStorage.setItem("userOutput", output)
+
+}
+
+// JSON.parse(localStorage.getItem("userOutput"))
+window.addEventListener('load', () => {
+ let userOutputs =  JSON.parse(localStorage.getItem('userOutputs') || '[]')
+    userOutputs.forEach((output) => {
         document.getElementById('details').innerHTML += output
     });
-
-    localStorage.setItem(userOutput, output)
-}
+})
 
 function validation(callback) {
     let info = document.querySelectorAll('.info')
-    for (let i = 0; i < info.length; i++) {
-        if (info[i].value == "" || info[i].value == null) {
-            info[i].nextElementSibling.innerHTML = info[i].previousElementSibling.innerHTML + " is required"
-        } else { info[i].nextElementSibling == "" }
-    }
-
-    dataArr = []
 
     let firstname = document.getElementById('firstname')
     let lastname = document.getElementById('lastname')
     let age = document.getElementById('age')
     let gender = document.getElementById('gender')
+    for (let i = 0; i < info.length; i++) {
+        if (info[i].value == "" || info[i].value == null) {
+            info[i].nextElementSibling.innerHTML = info[i].previousElementSibling.innerHTML + " is required"
+        } else { info[i].nextElementSibling == "" }
+
+    }
+    if (!firstname.value && !lastname.value && !age.value && !gender.value) {
+        return;
+    }
+
+
+    dataArr = []
     let obj = {
         Firstname: firstname.value,
         Lastname: lastname.value,
@@ -39,10 +57,6 @@ function validation(callback) {
     callback(dataArr)
 }
 
-
-// validation()
 document.getElementById('btn').onclick = () => {
     validation(userData)
-
-    // alert('working')
 }
