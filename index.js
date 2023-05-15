e9d913e2743f49dcdff75252654f2153a9946e45
TOKONIH <tokonihstandwell@gmail.com>
2023-05-15 12:31:28 +0100

    i modified my validation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+
 function userData(arr) {
-    let userOutputs = JSON.parse(localStorage.getItem('useOutputs') || '[]')
+    // let userOutputs = JSON.parse(localStorage.getItem('useOutputs') || '[]')
     let output = ""
     arr.forEach((data) => {
         output += `
@@ -10,27 +11,28 @@ function userData(arr) {
         <p>Gender: ${data.Gender}</p>
     </div> 
         `;
-        userOutputs.push(output)
-        document.getElementById('details').innerHTML += output
+        // userOutputs.push(output)
+        document.getElementById('details').innerHTML = output
 
     });
-    localStorage.setItem('userOutputs', JSON.stringify(userOutputs))
+    // localStorage.setItem('userOutputs', JSON.stringify(userOutputs))
     // localStorage.setItem('userOutput', JSON.stringify(output))
     // localStorage.setItem("userOutput", output)
 
 }
 
 // JSON.parse(localStorage.getItem("userOutput"))
-window.addEventListener('load', () => {
-    let userOutputs = JSON.parse(localStorage.getItem('userOutputs') || '[]')
-    userOutputs.forEach((output) => {
-        document.getElementById('details').innerHTML += output
-    });
-})
+// window.addEventListener('load', () => {
+//     let userOutputs = JSON.parse(localStorage.getItem('userOutputs') || '[]')
+//     userOutputs.forEach((output) => {
+//         document.getElementById('details').innerHTML += output
+//     });
+// })
 
-function validation(callback) {
+function validation(callback) { 
+
+
     let info = document.querySelectorAll('.info')
-
     let firstname = document.getElementById('firstname')
     let lastname = document.getElementById('lastname')
     let age = document.getElementById('age')
@@ -38,7 +40,7 @@ function validation(callback) {
     for (let i = 0; i < info.length; i++) {
         if (info[i].value == "" || info[i].value == null) {
             info[i].nextElementSibling.innerHTML = info[i].previousElementSibling.innerHTML + " is required"
-        } else { info[i].nextElementSibling == "" }
+        } else { info[i].nextElementSibling.innerHTML = "" }
     }
 
     if(!firstname.value || !lastname.value || !age.value || !gender.value){
@@ -46,18 +48,30 @@ function validation(callback) {
         return;
     }
 
-    dataArr = []
+    // dataArr = []
     let obj = {
         Firstname: firstname.value,
         Lastname: lastname.value,
         Age: age.value,
         Gender: gender.value,
-    }
-    dataArr.push(obj)
-    callback(dataArr)
+    };
+
+    let storedData = localStorage.getItem('tableContents');
+    let tableContents = storedData ? JSON.parse(storedData) : [];
+    tableContents.push(obj);
+    localStorage.setItem('tableContents', JSON.stringify(tableContents));
+    // dataArr.push(obj)
+
+    callback(tableContents);
 }
 
 document.getElementById('btn').onclick = (e) => {
     e.preventDefault()
     validation(userData)
-}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    let storedData = localStorage.getItem('tableContents');
+    let tableContents = storedData ? JSON.parse(storedData) : [];
+    userData(tableContents);
+})
